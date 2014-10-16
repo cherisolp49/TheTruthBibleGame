ae94c0b1919f28b852df3d2608694fce294e5db3
cherisolp49 <cherisolp49@gmail.com>
2014-10-16 00:04:32 -0400

    Updated Players
    Added to the method increment points. Set up means of determining how many points need to be given for each question. Contains the view results method for each player.

--- src/Players.java
+++ src/Players.java
@@ -1,13 +1,59 @@
-public class Players 
+package Gameplay;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Players
 {
     private String playerName;      
     private int points;         
     private char currentAnswer;
-            
+    private int numOfPlayers;
+    Date date = new Date();
+    SimpleDateFormat ft = new SimpleDateFormat("EMM.dd.yyyy'['hh.mm.ssa']'");
+    String t = ft.format(date) + "";
+    File f = new File("C:/Users/Patrick/Documents/NetBeansProjects/TheTruthBibleGame/viewResults/" + t + ".txt");        
     public Players(String name)
     {
+        System.out.println(name);
         this.playerName = name;
         this.points = 0;
+    }
+//    public void info(int numPlayers)
+//    {
+//        for(int i=0; i<numPlayers; i++)
+//        {
+//            String name = GetData.getWord("Enter Player " + i+1 + " name: ");
+//            
+//        }
+//    }
+    /*
+     *This method is to get the results of the player to see where it is that they went wrong
+     *and to learn from their mistakes. This method should be sent to a text file.
+    */
+    public void viewResults(String result)
+    {
+        System.out.println(t + "kjhbkhbjhbjbhj");
+        try{
+//            if(!f.exists()){
+//    			f.createNewFile();
+//    		}
+            FileWriter fw = new FileWriter(f.getName(),true);
+            fw.write(result + System.getProperty("line.separator"));    //line separator creates a new line
+            fw.close();
+        }catch(Exception ex){
+            
+        }
+        try{
+            FileWriter fw = new FileWriter("TestingTextFile.txt");
+            PrintWriter pw = new PrintWriter(fw);
+            
+            pw.println(result);
+            pw.close();    
+        }catch (IOException e) {
+            System.out.println("ERROR!");
+        }
     }
     public String getPlayerName()
     {
@@ -21,8 +67,21 @@ public class Players
     {
         return currentAnswer;
     }
-    public void incrementPoints(int points)
+    public void incrementPoints(String levelOfDiff)
     {
-        this.points = this.points + points;
+        System.out.println("testing!!!! " + levelOfDiff);
+        if(levelOfDiff.equalsIgnoreCase("EASY"))    //easy questions are worth 25 points
+        {
+            this.points = this.points + 25;
+        }
+        else if(levelOfDiff.equalsIgnoreCase("MEDIUM"))
+        {
+            this.points = this.points + 35;
+        }
+        else if(levelOfDiff.equalsIgnoreCase("HARD"))
+        {
+            this.points = this.points + 50;
+        }
     }
+
 }
